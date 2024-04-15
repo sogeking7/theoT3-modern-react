25793fcc9d81a853796053cc68cef5a9bc6338fc
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-04-16 02:26:13 +0500

    delete post model, create image model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,21 @@ import { db } from "~/server/db";
 // next-js auto complete
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/0516ff63-2aed-4a6b-9e07-bd4a57dc1d7c-vshqfz.png",
-  "https://utfs.io/f/bb34480d-1711-44ff-86a3-d107e320489b-z2p4d9.png",
-  "https://utfs.io/f/267286cb-09fc-4dfb-b654-99598241cf66-ozg89o.avif",
-  "https://utfs.io/f/d62747af-673e-4d87-b394-01ade1b9bef6-fe66e0.jpg",
-  "https://utfs.io/f/bde7d019-9f87-414b-8e34-aace7960b2ea-eii58c.avif",
-];
-
-const mockData = mockUrls.map((url, index) => {
-  return {
-    id: index + 1,
-    url,
-    title: `Image ${index + 1}`,
-  };
-});
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => asc(model.name),
+  });
 
-  console.log(posts);
+  console.log(images); // only shows on the server.
 
   return (
     <main>
-      <div>
-        {posts.map((post) => {
-          return (
-            <div key={post.id} className="">
-              {post.name}
-            </div>
-          );
-        })}
-      </div>
       <div className="flex gap-4 flex-wrap">
-        {[...mockData, ...mockData, ...mockData].map((data, index) => {
+        {images.map((data) => {
           return (
-            <div key={data.id + "-" + index} className="w-1/4 p-4">
-              <img src={data.url} alt={data.title} />
+            <div key={data.id} className="w-1/4 p-4">
+              <img src={data.url} alt={data.name} />
+              <h2>{data.name}</h2>
             </div>
           );
         })}
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,11 +18,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `theoT3-modern-react_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "image",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1025 }).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
