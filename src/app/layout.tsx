70f199c637d15375e1334d6d96179d5a3c9b5219
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-04-16 02:45:48 +0500

    add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { TopNav } from "./_components/TopNav";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,8 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>
+          <TopNav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
