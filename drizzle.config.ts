311e1f3e52f5383b81098d3571aeb6667cfa84b8
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-04-16 02:14:41 +0500

    set up drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: env.POSTGRES_URL,
   },
   tablesFilter: ["theoT3-modern-react_*"],
 } satisfies Config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,50 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+// next-js auto complete
+export const dynamic = "force-dynamic";
+
+const mockUrls = [
+  "https://utfs.io/f/0516ff63-2aed-4a6b-9e07-bd4a57dc1d7c-vshqfz.png",
+  "https://utfs.io/f/bb34480d-1711-44ff-86a3-d107e320489b-z2p4d9.png",
+  "https://utfs.io/f/267286cb-09fc-4dfb-b654-99598241cf66-ozg89o.avif",
+  "https://utfs.io/f/d62747af-673e-4d87-b394-01ade1b9bef6-fe66e0.jpg",
+  "https://utfs.io/f/bde7d019-9f87-414b-8e34-aace7960b2ea-eii58c.avif",
+];
+
+const mockData = mockUrls.map((url, index) => {
+  return {
+    id: index + 1,
+    url,
+    title: `Image ${index + 1}`,
+  };
+});
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+    <main>
+      <div>
+        {posts.map((post) => {
+          return (
+            <div key={post.id} className="">
+              {post.name}
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+          );
+        })}
       </div>
+      <div className="flex gap-4 flex-wrap">
+        {[...mockData, ...mockData, ...mockData].map((data, index) => {
+          return (
+            <div key={data.id + "-" + index} className="w-1/4 p-4">
+              <img src={data.url} alt={data.title} />
+            </div>
+          );
+        })}
+      </div>
+      <h1>Hello</h1>
     </main>
   );
 }
