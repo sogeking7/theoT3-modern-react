a86c6ffb1150b99b4371258c7defde15354ed6ae
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-04-16 01:27:26 +0500

    starting point

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+  tablesFilter: ["theoT3-modern-react_*"],
+} satisfies Config;
